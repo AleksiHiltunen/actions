4fc1cb2cb38f5f11926e78029db571096b04f81f
Aleksi Hiltunen <aleksi.hiltunen@moflixgroup.com>
2022-11-23 13:16:11 +0200

    add epoch to timestamp

--- check_timestamp.py
+++ check_timestamp.py
@@ -1,8 +1,10 @@
 import sys
+import time
 
-def main(timestamp):
-    print("timestamp: {}".format(timestamp))
-    assert False, {timestamp}
+def main(epoch):
+    print("timestamp: {}".format(epoch))
+    gmtime = time.strftime('%Y-%m-%dT%H:%M:%SZ', time.gmtime(int(epoch)))
+    print("Commit pushed at: {}".format(gmtime))
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
